0bf63e0d72b47e1936364651382a4e3ed485f35f
tasim313 <mostasimmahmudtasim@gmail.com>
2022-09-12 13:46:12 +0600

    authentication, social_authentication

---
+++ account/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path,include
+from django.contrib.auth import views as auth_views
+from .views import *
+
+app_name = 'account'
+
+urlpatterns = [
+    path('login/', user_login, name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('', include('django.contrib.auth.urls')),
+    path('register/', register, name='register'),
+    path('edit/', edit, name='edit'),
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    # path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    # path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+]
